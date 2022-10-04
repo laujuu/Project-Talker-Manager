30e8cf187b815776f93d51f32388824ddeb29814
laujuu <lauroindcontato@gmail.com>
2022-10-04 14:42:29 -0300

    Cria o endpoint GET /talker/:id

--- src/routes/talker.router.js
+++ src/routes/talker.router.js
@@ -5,6 +5,7 @@ const talkerRouter = express.Router();
 const fs = require('fs/promises');
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 const TALKER_FILE = './src/talker.json';
 
 talkerRouter.get('/talker', async (_req, res) => {
@@ -12,5 +13,17 @@ talkerRouter.get('/talker', async (_req, res) => {
   if (talkers.length === 0) return res.status(HTTP_OK_STATUS).send([]);
   return res.status(HTTP_OK_STATUS).json(JSON.parse(talkers));
 });
+
+talkerRouter.get('/talker/:id', async (req, res) => {
+    const { id } = req.params;
+    const talkers = JSON.parse(await fs.readFile(TALKER_FILE));
+    const findTalker = talkers.find((talker) => talker.id === Number(id));
+    if (findTalker) {
+      return res.status(HTTP_OK_STATUS).send(findTalker);
+    }
+    res
+      .status(HTTP_NOT_FOUND_STATUS)
+      .json({ message: 'Pessoa palestrante não encontrada' });
+  });
 
 module.exports = talkerRouter;
